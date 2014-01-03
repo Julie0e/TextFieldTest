a9827950318dd03fc22b7a9c5170dc9f8e54469c
Julie0e <julie_91@hanmail.net>
2014-01-03 17:09:20 +0900

    TextFieldTest 완료했습니

--- TextFieldTest/ViewController.m
+++ TextFieldTest/ViewController.m
@@ -10,20 +10,36 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+
+
 @end
 
 @implementation ViewController
 
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [self.textField becomeFirstResponder];
+}
+
+
+-(IBAction)dismissKeyboard:(id)sender
+{
+    [self.textField resignFirstResponder];
+    
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   
 }
 
 @end
